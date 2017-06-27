34b7c157c6c688d57df81c23f93019fc0c2b40b6
jky142 <jky142@naver.com>
2017-06-27 10:28:35 +0900

    hotfix branch change a code

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -34,6 +34,7 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		System.out.println("Hello Git");
+		System.out.println("nigiral Ssibal ro ma");		
 		
 		return "home";
 	}
