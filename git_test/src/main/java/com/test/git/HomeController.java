10b95c9bfc856d7785fc51f6ae9ea1625d63798e
jky142 <jky142@naver.com>
2017-06-27 10:22:08 +0900

    master branch change a code

--- git_test/src/main/java/com/test/git/HomeController.java
+++ git_test/src/main/java/com/test/git/HomeController.java
@@ -34,6 +34,7 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		System.out.println("Hello Git");
+		System.out.println("Conflict Ssibal ro ma");
 		
 		return "home";
 	}
